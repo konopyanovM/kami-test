f3605ed60b073a341279d60889c7f3c28ffe4580
Manap <konopyanov.manap@gmail.com>
2022-09-12 18:25:38 +0600

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Page from './modules/Page'
+import { PagesEnum } from './pages'
 
 function App() {
   return (
     <div className='App'>
       <Page>
-        <></>
+        <Routes>
+          <Route path={PagesEnum.products}></Route>
+        </Routes>
       </Page>
     </div>
   )
---
+++ src/pages/PagesEnum.ts
@@ -0,0 +1,3 @@
+export const enum PagesEnum {
+  products = '/products',
+}
